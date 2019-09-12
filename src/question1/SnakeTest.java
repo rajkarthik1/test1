79bd4e2303fc02180a338d4b35e29b91801c30de
KarthikRaj <rajkarthik711@gmail.com>
2019-09-12 13:00:34 -0400

    final

---
+++ src/question1/SnakeTest.java
@@ -0,0 +1,46 @@
+package question1;
+
+import static org.junit.Assert.*;
+
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class SnakeTest {
+
+	private static final String String = null;
+
+	@Before
+	public void setUp() throws Exception {
+		
+		
+		
+		
+
+		
+	
+	@Test
+public void isHealthy() {
+		String name = "Takis";
+		
+		String Favfood = "vegetables";
+		String c = name.concat(Favfood);
+		assertEquals("isHealthy",c);
+		
+	}
+	}
+
+		
+		
+	
+			
+		
+		}
+
+
+		
+	
+
+
+
+
